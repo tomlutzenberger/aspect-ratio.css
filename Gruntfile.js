3849188064c2bd4c4b84fa94b79d6ac4be64b1f9
Tom Lutzenberger <tl@frontal.ch>
2016-08-31 21:39:26 +0200

    Prepare project

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+
+/*global module:false*/
+
+module.exports = function (grunt) {
+
+  'use strict';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // watch task
+    watch: {
+      // observe all *.css files and lint them
+      css: {
+        files: 'dist/*.css',
+        tasks: ['csslint'],
+        options: {}
+      },
+      // observe all *.less files, execute less task on change and reload the site
+      less: {
+        files: 'less/*.less',
+        tasks: ['less'],
+        options: {}
+      }
+    },
+
+    less: {
+      development: {
+        options: {
+          compress: false
+        },
+        files: {
+          'dist/aspect-ratio.css': 'less/aspect-ratio.less'
+        }
+      },
+      productive: {
+        options: {
+          compress: true
+        },
+        files: {
+          'dist/aspect-ratio.min.css': 'less/aspect-ratio.less'
+        }
+      }
+    },
+
+    csslint: {
+      options: {
+        csslintrc: '.csslintrc'
+      },
+      strict: {
+        options: {
+          import: 2
+        },
+        src: 'dist/*.css'
+      },
+      lax: {
+        options: {
+          import: false
+        },
+        src: 'dist/*.css'
+      }
+    }
+  });
+
+  // Tasks registrieren
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+
+
+  grunt.registerTask('default', 'watch');
+
+};
